7c32c18b7ea12acbd5216be198fd7f1772729879
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-11-21 14:15:01 +0100

    Milestone 2

--- src/generation/italy/ticketOffice/TicketOffice.java
+++ src/generation/italy/ticketOffice/TicketOffice.java
@@ -1,9 +1,16 @@
 package generation.italy.ticketOffice;
 
+import java.util.Scanner;
+
 public class TicketOffice {
     public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Inserire il numero di km:");
+        int km = sn.nextInt();
+        System.out.println("Inserire l'età:");
+        int age = sn.nextInt();
         try {
-            Ticket ticket = new Ticket(-100, 17);
+            Ticket ticket = new Ticket(km, age);
             System.out.println(ticket);
         } catch (Exception e) {
             System.err.println(e.getMessage());
